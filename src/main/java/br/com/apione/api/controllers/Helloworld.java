c454cf334032069d12aa733e6e0e9cda86cee218
Gabriel <gabrielalmeidads@gmail.com>
2023-02-23 15:23:28 -0300

    Updating for gateway connection

--- src/main/java/br/com/apione/api/controllers/Helloworld.java
+++ src/main/java/br/com/apione/api/controllers/Helloworld.java
@@ -1,18 +1,29 @@
 package br.com.apione.api.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 @RequestMapping(value = "/hello")
 public class Helloworld {
 
+	@Autowired
+	RestTemplate restTemplate;
 
 	@RequestMapping(method = RequestMethod.GET)
 	public ResponseEntity<String> findAll() {
 		return ResponseEntity.ok().body("Hello world!");
+	}
+	
+	@RequestMapping(value = "/apitwo", method = RequestMethod.GET)
+	public ResponseEntity<String> findAll2() {
+		String res = restTemplate.getForObject("http://apitwo-service/apitwo/two", String.class);
+		return ResponseEntity.ok().body(res);
+		
 	}
 
 	
